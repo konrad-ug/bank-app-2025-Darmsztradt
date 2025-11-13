c84bf1379db9cba2c613eb7d3869f83a3ace0f5f
Tomasz Wanke <tomwan04@gmail.com>
2025-11-13 13:40:00 +0100

    History of payments

--- src/account.py
+++ src/account.py
@@ -1,11 +1,15 @@
 class Account:
     def __init__(self):
         self.balance = 0.0
-    
+        self.historia = []
+
     def incoming_transfer(self, amount):
         if amount > 0:
             self.balance += amount
+            self.historia.append(amount)
 
     def outgoing_transfer(self, amount):
         if amount > 0 and amount <= self.balance:
             self.balance -= amount
+            self.historia.append(-amount)
+
